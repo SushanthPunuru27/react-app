a32b07b6d021a86f53a07536bb98767076853f19
SushanthPunuru98 <punurusushanth@gmail.com>
2023-05-06 00:12:39 +0530

    EX-1

---
+++ App.js
@@ -0,0 +1,29 @@
+/*<div id ="parent">
+    <div id="child">
+        <h1>I am h1</h1>
+        <h2>I am h2</h2>
+    </div>
+    <div id="child2">
+        <h1>I am h1</h1>
+        <h2>I am h2</h2>
+    </div>
+</div>*/
+const parent = React.createElement(
+    "div",
+    { id: "parent" }, [
+    React.createElement(
+        "div",
+        { id: "child" },
+        [React.createElement("h1", {id: "heading"}, "I am H!"), React.createElement("h2", {}, "I am h2")]
+    ), React.createElement(
+        "div",
+        { id: "child2" },
+        [React.createElement("h1", {id : "heading"}, "I am H!"), React.createElement("h2", {}, "I am h2")]
+    )
+]
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
